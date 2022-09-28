309ccfb2cdd6ac4a161377215a7dfd1753c7b66a
Santiago Gomez <santiago.daniel.gomez@gmail.com>
2022-09-28 15:11:36 -0300

    4to Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { Component } from 'react'; 
 import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
-import Brand from './components/Brand';
-// import './app.css'
 
 const App = () => {
   return (
     <div className='container'>
-      {/* {brand} */}
-      {/* <Brand>Autocor</Brand> */}
       <Navbar />
       <ItemListContainer greeting='Bienvenidos a nuestro Catálogo de Productos'/>
       <div className='fondo'></div>
--- src/components/Brand.js
+++
@@ -1,7 +0,0 @@
-const Brand = ({props}) => {
-    return(
-        <h1>{props.childen}</h1>
-    )
-}
-
-export default Brand;
